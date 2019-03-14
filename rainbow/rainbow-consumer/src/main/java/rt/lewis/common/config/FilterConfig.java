7cacfc562a90fefb8d9251eed6225af74b12126b
Lulei <461770903@qq.com>
2019-03-14 21:38:11 +0800

    过滤器

---
+++ rainbow/rainbow-consumer/src/main/java/rt/lewis/common/config/FilterConfig.java
@@ -0,0 +1,19 @@
+package rt.lewis.common.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import rt.lewis.common.filter.CORSFilter;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean(){
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.addUrlPatterns("/**");
+        CORSFilter filter = new CORSFilter();
+        registration.setFilter(filter);
+        registration.setName("corsFilter");
+        return registration;
+    }
+}
--- rainbow/rainbow-consumer/src/main/java/rt/lewis/common/config/WebConfig.java
+++ rainbow/rainbow-consumer/src/main/java/rt/lewis/common/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -43,4 +44,22 @@ public class WebConfig implements WebMvcConfigurer {
                 .modulesToInstall(new ParameterNamesModule());
         converters.add(new MappingJackson2HttpMessageConverter(builder.build()));
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+    //responce.set('Access-Control-Allow-Origin', request.get('origin'));
+
+/*
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://domain2.com")
+                .allowedMethods("PUT", "DELETE")
+                .allowedHeaders("header1", "header2", "header3")
+                .exposedHeaders("header1", "header2")
+                .allowCredentials(false).maxAge(3600);
+    }*/
 }
